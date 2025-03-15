8e4a707c4ac090516e2e22c8e718b742cc4173ad
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-03-15 17:33:25 +0000

    Add zstd package

--- src/compile-debug.bat
+++ src/compile-debug.bat
@@ -1,3 +1,11 @@
-cmake -S . -B build-x86-debug -DBUILD_SHARED_LIBS=OFF -DUSE_STATIC_RUNTIME=ON -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_BUILD_TYPE=Debug -A Win32
+@rem -*- mode: bat -*-
+@echo off
+
+cmake -S . -B build-x86-debug ^
+  -DBUILD_SHARED_LIBS=OFF -DUSE_STATIC_RUNTIME=ON -DCMAKE_BUILD_TYPE=Debug -A Win32
+
+rem cmake -S . -B build-x86-debug ^
+rem   -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static ^
+rem   -DBUILD_SHARED_LIBS=OFF -DUSE_STATIC_RUNTIME=ON -DCMAKE_BUILD_TYPE=Debug -A Win32
 
 cmake --build build-x86-debug --config Debug
--- src/lib/include/lvzstd/lvzstd.h
+++ src/lib/include/lvzstd/lvzstd.h
@@ -1,1 +1,3 @@
+#include "sg/export/
+
 int test();
