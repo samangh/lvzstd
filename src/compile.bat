acda6658df2a4fd66c737b87a545d27cee5f264b
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-06-02 00:17:39 +0100

    Update compile script

--- src/compile.bat
+++ src/compile.bat
@@ -1,11 +1,18 @@
 @rem -*- mode: bat -*-
 @echo off
 
-cmake -S . -B build-x86 ^
+set X86_DIR=build-x86
+set X64_DIR=build-x86_64
+
+cmake -S . -B %X86_DIR% ^
   -DBUILD_SHARED_LIBS=OFF -DUSE_STATIC_RUNTIME=ON -DCMAKE_BUILD_TYPE=Release -A Win32
 
-cmake -S . -B build-x86_64 ^
+cmake -S . -B %X64_DIR% ^
   -DBUILD_SHARED_LIBS=OFF -DUSE_STATIC_RUNTIME=ON -DCMAKE_BUILD_TYPE=Release
 
-cmake --build build-x86    --config Release
-cmake --build build-x86_64 --config Release
+cmake --build %X86_DIR%    --config Release
+cmake --build %X64_DIR% --config Release
+
+copy %X64_DIR%\lib\Release\lvzstd.dll ..\libs\lvzstd-x86_64.dll
+copy %X86_DIR%\lib\Release\lvzstd.dll ..\libs\lvzstd-x86.dll
+
